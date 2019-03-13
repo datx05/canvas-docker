2943251689d4ee97e01d41d960f333728ba77ee0
Niklas <me@sodapop.se>
2019-03-13 17:04:45 +0100

    Fix directory fix sed replacements

--- assets/start.sh
+++ assets/start.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-sed -i "s/EMAIL_DELIVERY_METHOD/${EMAIL_DELIVERY_METHOD-test}/" ${APP_HOME}config/outgoing_mail.yml
-sed -i "s/SMTP_ADDRESS/${SMTP_ADDRESS-localhost}/" ${APP_HOME}config/outgoing_mail.yml
-sed -i "s/SMTP_PORT/${SMTP_PORT-25}/" ${APP_HOME}config/outgoing_mail.yml
-sed -i "s/SMTP_USER/${SMTP_USER-}/" ${APP_HOME}config/outgoing_mail.yml
-sed -i "s/SMTP_PASS/${SMTP_PASS-}/" ${APP_HOME}config/outgoing_mail.yml
+sed -i "s/EMAIL_DELIVERY_METHOD/${EMAIL_DELIVERY_METHOD-test}/" ${APP_HOME}canvas-lms/config/outgoing_mail.yml
+sed -i "s/SMTP_ADDRESS/${SMTP_ADDRESS-localhost}/" ${APP_HOME}canvas-lms/config/outgoing_mail.yml
+sed -i "s/SMTP_PORT/${SMTP_PORT-25}/" ${APP_HOME}canvas-lms/config/outgoing_mail.yml
+sed -i "s/SMTP_USER/${SMTP_USER-}/" ${APP_HOME}canvas-lms/config/outgoing_mail.yml
+sed -i "s/SMTP_PASS/${SMTP_PASS-}/" ${APP_HOME}canvas-lms/config/outgoing_mail.yml
 
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
