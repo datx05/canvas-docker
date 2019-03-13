4bff7ca6272e66ad2aae0f60053fc2879d42bf28
Niklas <me@sodapop.se>
2019-03-13 22:14:11 +0100

    Postgres service and supervisord directories

--- assets/start.sh
+++ assets/start.sh
@@ -8,4 +8,6 @@ sed -i "s/SMTP_PORT/${SMTP_PORT-25}/" ${APP_HOME}canvas-lms/config/outgoing_mail
 sed -i "s/SMTP_USER/${SMTP_USER-}/" ${APP_HOME}canvas-lms/config/outgoing_mail.yml
 sed -i "s/SMTP_PASS/${SMTP_PASS-}/" ${APP_HOME}canvas-lms/config/outgoing_mail.yml
 
+sudo service postgresql start
+
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
